c7833b9feefcaa107f65767ac828210d0b06cf13
Chen Lei <chenlei-iri@360.cn>
2014-12-11 18:46:48 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 TAG=${1:-`git describe --tags --abbrev=0 master`}
 VER=${TAG#v*}
+PKG_NAME=php-trace
 echo pack tag $TAG
-git archive --format=tar --prefix=phptrace-$VER/ $TAG | gzip > phptrace-$VER.tar.gz
+git archive --format=tar --prefix=$PKG_NAME-$VER/ $TAG | gzip > $PKG_NAME-$VER.tar.gz
