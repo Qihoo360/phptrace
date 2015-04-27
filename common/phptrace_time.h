b6ba077980fd3a48b3a338a6a95cf3ddf907670e
Yuchen Wang <phobosw@gmail.com>
2015-04-27 21:52:09 +0800

    Change time functions to static inline

--- common/phptrace_time.h
+++ common/phptrace_time.h
@@ -1,6 +1,25 @@
 #ifndef PHPTRACE_TIME_H
 #define PHPTRACE_TIME_H
+
 #include <stdint.h>
-uint64_t phptrace_time_usec();
-uint64_t phptrace_cputime_usec();
+#include <sys/resource.h>
+#include <sys/time.h>
+
+static inline uint64_t phptrace_time_usec()
+{
+    struct timeval tv;
+    gettimeofday(&tv, 0);
+    return (uint64_t)tv.tv_sec * 1000000 + (uint64_t)tv.tv_usec;
+}
+
+static inline uint64_t phptrace_cputime_usec()
+{
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    return (uint64_t)usage.ru_utime.tv_sec * 1000000 +
+           (uint64_t)usage.ru_utime.tv_usec +
+           (uint64_t)usage.ru_stime.tv_sec * 1000000 +
+           (uint64_t)usage.ru_stime.tv_usec;
+}
+
 #endif
