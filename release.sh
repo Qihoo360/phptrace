fc0c819b2d2da0b34f690680404e587f1adfa1ba
Yuchen Wang <phobosw@gmail.com>
2015-05-30 00:13:14 +0800

    Remove release.sh script

--- release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TAG=${1:-`git describe --tags --abbrev=0 master`}
-VER=${TAG#v*}
-PKG_NAME=php-trace
-echo pack tag $TAG
-git archive --format=tar --prefix=$PKG_NAME-$VER/ $TAG | gzip > $PKG_NAME-$VER.tar.gz
